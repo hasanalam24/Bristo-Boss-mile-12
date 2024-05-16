02f6418339aeaea236b176374c615e130bd362f1
hasanalam24 <hasanalam365@gmail.com>
2024-05-16 22:54:01 +0600

    parallax bg img style

--- src/Pages/Home/Features/Features.jsx
+++ src/Pages/Home/Features/Features.jsx
@@ -5,14 +5,14 @@ import './Features.css'
 
 const Features = () => {
     return (
-        <div className="featured-item text-white pt-8 my-16">
+        <div className="featured-item text-white pt-8 my-16 bg-fixed">
 
             <SectionTitle
                 heading={"Featured Item"}
                 subHeading={"Check it Out"}
             ></SectionTitle>
 
-            <div className="md:flex justify-center items-center py-16 px-36">
+            <div className="md:flex justify-center items-center py-16 px-36 bg-slate-500 bg-opacity-40">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -20,7 +20,7 @@ const Features = () => {
                     <p>May 16,2024</p>
                     <p className="uppercase">Where can i get some </p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dignissimos officiis aspernatur beatae esse fuga, aut odit voluptates pariatur. Ab voluptate fugit, facilis rem quam tempora suscipit nisi qui odio.</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4">Order Now</button>
                 </div>
             </div>
 
