094f2e44bc955cf057aaafc8a9a3ec81cee86898
hasanalam24 <hasanalam365@gmail.com>
2024-05-16 22:14:38 +0600

    banner navber category done

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+
+
+const PopularMenu = () => {
+    return (
+        <div>
+            <SectionTitle></SectionTitle>
+        </div>
+    );
+};
+
+export default PopularMenu;
