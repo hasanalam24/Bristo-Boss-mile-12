39fa54a715beb897ca651a1df1e4b8ca345c125b
hasanalam24 <hasanalam365@gmail.com>
2024-05-18 06:05:02 +0600

    pagination food item show using swiper pagination

--- src/Components/FoodCart/FoodCart.jsx
+++ src/Components/FoodCart/FoodCart.jsx
@@ -7,11 +7,11 @@ const FoodCart = ({ item }) => {
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
             <p className="absolute right-5 top-5 bg-slate-900 text-white font-medium py-1 px-3">${price}</p>
-            <div className="card-body text-center">
-                <h2 className="card-title">{name}</h2>
+            <div className="card-body flex flex-col items-center">
+                <h2 className="card-title ">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Add to Cart</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-200 border-orange-400">Add to Cart</button>
                 </div>
             </div>
         </div>
--- src/Pages/OrderFood/OrderCart/OrderCart.jsx
+++ src/Pages/OrderFood/OrderCart/OrderCart.jsx
@@ -1,11 +1,46 @@
 import FoodCart from "../../../Components/FoodCart/FoodCart";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 const OrderCart = ({ items }) => {
+
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    };
+
     return (
-        <div className='grid md:grid-cols-3 gap-10'>
-            {
-                items.map(item => <FoodCart key={item._id} item={item}></FoodCart>)
-            }
+
+        <div>
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className='grid md:grid-cols-3 gap-10'>
+                        {
+                            items.slice(0, 6).map(item => <FoodCart key={item._id} item={item}></FoodCart>)
+                        }
+                    </div>
+                </SwiperSlide>
+                {
+                    items.length > 6 && <SwiperSlide>
+                        <div className='grid md:grid-cols-3 gap-10'>
+                            {
+                                items.slice(6, items.length).map(item => <FoodCart key={item._id} item={item}></FoodCart>)
+                            }
+                        </div>
+                    </SwiperSlide>
+                }
+
+            </Swiper>
         </div>
     );
 };
