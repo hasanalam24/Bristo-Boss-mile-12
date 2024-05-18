0078dd93b37da7367090875925f3eefbf9ffce81
hasanalam24 <hasanalam365@gmail.com>
2024-05-18 19:40:23 +0600

    login and navbar and footer hide when user click login page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import MenuMain from "../Pages/Menu/MenuMain/MenuMain";
 import Order from "../Pages/OrderFood/Order/Order";
+import Login from "../Pages/Login/Login";
 
 export const router = createBrowserRouter([
     {
@@ -20,7 +21,11 @@ export const router = createBrowserRouter([
             {
                 path: '/order/:category',
                 element: <Order></Order>
-            }
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
         ]
     },
 ]);
