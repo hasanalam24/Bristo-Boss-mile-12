74062cfd3f231ab8600b2032f5b4150110d6a066
hasanalam24 <hasanalam365@gmail.com>
2024-05-19 20:14:49 +0600

    private route secret page only navigate login page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,8 @@ import MenuMain from "../Pages/Menu/MenuMain/MenuMain";
 import Order from "../Pages/OrderFood/Order/Order";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import Secret from "../Shared/Secret/Secret";
+import PrivateRoute from "./PrivateRoute/PrivateRoute";
 
 export const router = createBrowserRouter([
     {
@@ -31,6 +33,12 @@ export const router = createBrowserRouter([
                 path: '/register',
                 element: <Register></Register>
             },
+            {
+                path: '/secret',
+                element: <PrivateRoute>
+                    <Secret></Secret>
+                </PrivateRoute>
+            }
         ]
     },
 ]);
