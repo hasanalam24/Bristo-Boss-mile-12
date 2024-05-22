e16261937ffcd0a63c445bcc106ae2f8d1ef593b
hasanalam24 <hasanalam365@gmail.com>
2024-05-23 01:29:40 +0600

    all user send to token from localstorage to server using headers

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../Firebase/firebase.config";
+import useAxiosCommon from "../Hooks/useAxiosCommon";
 export const AuthContext = createContext(null)
 const auth = getAuth(app);
 
@@ -10,6 +11,7 @@ const googleProvider = new GoogleAuthProvider()
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
+    const axiosCommon = useAxiosCommon()
 
     //create user
     const signUpUser = (email, password) => {
@@ -49,6 +51,21 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unSubcribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
+
+            if (currentUser) {
+                //get token and store client
+                const userInfo = { email: currentUser.email }
+                axiosCommon.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token)
+                        }
+                    })
+            }
+            else {
+                //TODO: remove token (if token stored in the client side: Local storage,caching,in memory)
+                localStorage.removeItem('access-token')
+            }
             setLoading(false)
         })
         return () => {
