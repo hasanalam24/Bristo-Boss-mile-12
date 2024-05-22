9f067e05d795456fcf74633344c746b7a3b091a3
hasanalam24 <hasanalam365@gmail.com>
2024-05-22 23:09:32 +0600

    delete func admin user

--- src/Pages/Dashbord/AllUsers/AllUsers.jsx
+++ src/Pages/Dashbord/AllUsers/AllUsers.jsx
@@ -1,22 +1,96 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { FaTrash, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
 
     const axiosSecure = useAxiosSecure()
-    const { data: users } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users')
             return res.data
         }
     })
+    // console.log(users.length)
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/users/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+
+                            });
+                        }
+                    })
+            }
+        });
+    }
+
 
     return (
         <div>
             <div className="flex justify-evenly my-4">
                 <h1 className="text-3xl">All Users:</h1>
                 <h1 className="text-3xl">Total Users:{users.length}</h1>
+            </div>
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table table-zebra w-full">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                users.map((user, idx) => <tr key={user._id}>
+                                    <th>{idx + 1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+
+                                    <td>
+                                        <button
+
+                                            className="btn bg-orange-500 btn-md ">
+                                            <FaUsers className="text-white text-2xl"></FaUsers>
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button
+                                            onClick={() => handleDelete(user._id)}
+                                            className="btn btn-ghost btn-lg text-red-600">
+                                            <FaTrash></FaTrash>
+                                        </button>
+                                    </td>
+                                </tr>)
+                            }
+
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
