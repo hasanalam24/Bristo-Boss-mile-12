4ea858c0be634637b24440e97b44024428b80688
hasanalam24 <hasanalam365@gmail.com>
2024-05-23 09:53:43 +0600

    error handle in the intercepts ,check error status 401/403

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -21,13 +21,26 @@ const useAxiosSecure = () => {
 
     axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access-token')
-        console.log('interceptors', token)
+        // console.log('interceptors', token)
         config.headers.authorization = `Bearer ${token}`
         return config;
     }, function (error) {
         //Do Something with request error
         return Promise.reject(error)
+    });
+
+
+    //intercepts 401 and 403 status
+    axiosSecure.interceptors.response.use(function (response) {
+        return response;
+    }, function (error) {
+        // console.log('error in the intercepts',error)
+        const status = error.response.status;
+        console.log('error in the intercepts', status)
+        return Promise.reject(error)
     })
+
+
     return axiosSecure
 };
 
