364799c2457947dd6896cb8d8037d2e81de5cf26
hasanalam24 <hasanalam365@gmail.com>
2024-05-28 23:25:49 +0600

    admin home data load

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -24,6 +24,7 @@ const useAxiosSecure = () => {
     const navigate = useNavigate()
     const { signOutUser } = useAuth()
 
+    //request interceptor to add authorization header for every secure call to the api
     axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access-token')
         // console.log('interceptors', token)
