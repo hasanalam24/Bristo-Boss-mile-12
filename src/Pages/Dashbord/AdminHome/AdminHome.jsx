b13659c7b2abe0a3e91402234aafc689f17e8f88
hasanalam24 <hasanalam365@gmail.com>
2024-05-28 21:24:54 +0600

    admin and user conditional dashboard show

--- src/Pages/Dashbord/AdminHome/AdminHome.jsx
+++ src/Pages/Dashbord/AdminHome/AdminHome.jsx
@@ -4,16 +4,19 @@ const AdminHome = () => {
 
     const { user } = useAuth()
 
+
     return (
         <div>
             <h1 className="text-3xl">
-                <span>Hi, Welcome</span>
-                {
-                    user?.displayName ? user.displayName : 'Back'
-                }
-                admin
+                <span>Hi, Welcome </span>
+                <span className={`${user && 'text-orange-600 font-semibold'}`}>
+                    {
+                        user?.displayName ? user.displayName : 'Back'
+                    }
+                </span>
+
             </h1>
-        </div>
+        </div >
     );
 };
 
--- src/Pages/Dashbord/UserHome/UserHome.jsx
+++ src/Pages/Dashbord/UserHome/UserHome.jsx
@@ -8,11 +8,13 @@ const UserHome = () => {
     return (
         <div>
             <h1 className="text-3xl">
-                <span>Hi, Welcome</span>
-                {
-                    user?.displayName ? user.displayName : 'Back'
-                }
-                user
+                <span>Hi, Welcome </span>
+                <span className={`${user && 'text-orange-600 font-semibold'}`}>
+                    {
+                        user?.displayName ? user.displayName : 'Back'
+                    }
+                </span>
+
             </h1>
         </div>
     );
