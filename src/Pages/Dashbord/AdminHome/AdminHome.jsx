d8e33582aaac25a0fcdc25ebc47c0d839a5738e3
hasanalam24 <hasanalam365@gmail.com>
2024-05-29 13:13:21 +0600

    recharts add

--- src/Pages/Dashbord/AdminHome/AdminHome.jsx
+++ src/Pages/Dashbord/AdminHome/AdminHome.jsx
@@ -3,6 +3,11 @@ import useAuth from '../../../Hooks/useAuth'
 import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 import { FaUsers } from 'react-icons/fa';
 
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+
 const AdminHome = () => {
 
     const { user } = useAuth()
@@ -29,6 +34,22 @@ const AdminHome = () => {
         return <span>Loading....</span>
     }
     // console.log(stats)
+
+
+    // custom shape for the BarCharts
+
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
 
     return (
         <div>
@@ -79,6 +100,38 @@ const AdminHome = () => {
                     <div className="stat-desc">↘︎ 90 (14%)</div>
                 </div>
 
+            </div>
+
+
+            {/* BarCharts */}
+            <div className="flex">
+                {/* customShape charts */}
+                <div className='w-1/2'>
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {chartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </div>
+                {/* circle barCharts */}
+                <div className='w-1/2'>
+
+                </div>
             </div>
         </div >
     );
