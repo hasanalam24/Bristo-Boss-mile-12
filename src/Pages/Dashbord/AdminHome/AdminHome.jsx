44adfe0783adabcfb3a7271f556a49f42ccf9de9
hasanalam24 <hasanalam365@gmail.com>
2024-05-29 13:27:02 +0600

    piechart added some customized code form react rechart exaple

--- src/Pages/Dashbord/AdminHome/AdminHome.jsx
+++ src/Pages/Dashbord/AdminHome/AdminHome.jsx
@@ -4,8 +4,10 @@ import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 import { FaUsers } from 'react-icons/fa';
 
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
 
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 
 const AdminHome = () => {
@@ -51,6 +53,24 @@ const AdminHome = () => {
         return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
     };
 
+
+    //custom pieCharts 
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+    const pieChartData = chartData.map(data => {
+        return { name: data.category, value: data.revenue }
+    })
     return (
         <div>
             <h1 className="text-3xl">
@@ -128,9 +148,26 @@ const AdminHome = () => {
                         </Bar>
                     </BarChart>
                 </div>
-                {/* circle barCharts */}
-                <div className='w-1/2'>
 
+
+                {/* PieChart/circle barCharts */}
+                <div className='w-1/2'>
+                    <PieChart width={400} height={400}>
+                        <Pie
+                            data={pieChartData}
+                            cx="50%"
+                            cy="50%"
+                            labelLine={false}
+                            label={renderCustomizedLabel}
+                            outerRadius={80}
+                            fill="#8884d8"
+                            dataKey="value"
+                        >
+                            {pieChartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                    </PieChart>
                 </div>
             </div>
         </div >
